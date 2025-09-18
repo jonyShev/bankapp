3ca7f313974e8cb341084276536e8b33bd708ac6
jonyShev <jony.a.shev@gmail.com>
2025-09-18 16:55:27 +0200

    init commit: created structure

---
+++ exchange-generator/src/test/java/com/jonyshev/exchange/generator/ExchangeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonyshev.exchange.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/jonyshev/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonyshev.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
