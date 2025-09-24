599dc1886087ed61d2d7eb9a63ed5e9c848592fa
jonyShev <jony.a.shev@gmail.com>
2025-09-24 16:59:55 +0200

    Added oauth-filter for exchange-generator

--- exchange-generator/src/main/java/com/jonyshev/exchange/generator/ExchangeGeneratorApplication.java
+++ exchange-generator/src/main/java/com/jonyshev/exchange/generator/ExchangeGeneratorApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
@@ -17,8 +20,21 @@ public class ExchangeGeneratorApplication {
 
     @Bean
     @LoadBalanced
-    WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    WebClient.Builder webClientBuilder(ServerOAuth2AuthorizedClientExchangeFilterFunction oauthFilter) {
+        return WebClient.builder().filter(oauthFilter);
     }
 
+    @Bean
+    ServerOAuth2AuthorizedClientExchangeFilterFunction oauthFilter(ReactiveClientRegistrationRepository clients,
+                                                                   ReactiveOAuth2AuthorizedClientService svc) {
+        var manager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clients, svc);
+        var provider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        manager.setAuthorizedClientProvider(provider);
+
+        var oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(manager);
+        oauth.setDefaultClientRegistrationId("exchange-generator");
+        return oauth;
+    }
 }
