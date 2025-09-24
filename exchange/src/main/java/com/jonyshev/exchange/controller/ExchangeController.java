668c2aa1823f3ed19e772fd3bf389c9bcccfc0d0
jonyShev <jony.a.shev@gmail.com>
2025-09-24 15:12:48 +0200

    Replaced RateStorage to RateService

--- exchange/src/main/java/com/jonyshev/exchange/controller/ExchangeController.java
+++ exchange/src/main/java/com/jonyshev/exchange/controller/ExchangeController.java
@@ -2,29 +2,31 @@ package com.jonyshev.exchange.controller;
 
 import com.jonyshev.commons.dto.RateDto;
 import com.jonyshev.commons.model.Currency;
-import com.jonyshev.exchange.core.RateStorage;
+import com.jonyshev.exchange.service.RateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ExchangeController {
 
-    private final RateStorage storage;
+    private final RateService rateService;
+
 
     // Для фронта: GET /api/rates -> список RateDto
     @GetMapping("/api/rates")
     public List<RateDto> getRates() {
-        return storage.toList();
+        return rateService.getAll();
     }
 
     // Для генератора: внутреннее обновление одного курса
     @PostMapping("/internal/rates/{code}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void updateRate(@PathVariable("code") String code, @RequestParam("value") double value) {
-        storage.put(Currency.valueOf(code), value);
+    public void updateRate(@PathVariable("code") String code, @RequestParam("value") BigDecimal value) {
+        rateService.upsert(Currency.valueOf(code), value);
     }
 }
