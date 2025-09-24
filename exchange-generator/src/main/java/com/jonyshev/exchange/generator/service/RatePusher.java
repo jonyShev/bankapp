699562d2b71e27eb12d3b8706811b73ba7f15283
jonyShev <jony.a.shev@gmail.com>
2025-09-24 15:11:18 +0200

    Renamed variables

--- exchange-generator/src/main/java/com/jonyshev/exchange/generator/service/RatePusher.java
+++ exchange-generator/src/main/java/com/jonyshev/exchange/generator/service/RatePusher.java
@@ -17,7 +17,6 @@ public class RatePusher {
 
     private final WebClient.Builder http;
 
-    // базовые значения
     private final Map<Currency, Double> base = Map.of(
             Currency.RUB, 1.0,
             Currency.USD, 90.0,
@@ -27,20 +26,19 @@ public class RatePusher {
     @Scheduled(fixedRate = 1000)
     @Retry(name = "push")
     public void tick() {
-        for (var e : base.entrySet()) {
-            var c = e.getKey();
-            double v = jiggle(e.getValue());
-            // идём прям в сервис exchange по имени (через Consul)
+        for (var entry : base.entrySet()) {
+            var key = entry.getKey();
+            double randomValue = generateRandomDouble(entry.getValue());
             http.build()
                     .post()
-                    .uri("http://exchange/internal/rates/{code}?value={v}", c.name(), v)
+                    .uri("http://exchange/internal/rates/{code}?value={value}", key.name(), randomValue)
                     .retrieve()
                     .toBodilessEntity()
                     .block();
         }
     }
 
-    private double jiggle(double base) {
+    private double generateRandomDouble(double base) {
         double delta = (ThreadLocalRandom.current().nextDouble() - 0.5) / 100.0;
         double val = base * (1.0 + delta);
         return Math.round(val * 100.0) / 100.0;
