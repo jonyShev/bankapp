f8cf6a87e3ff794da5f14cede6643a56c8d6f45a
jonyShev <jony.a.shev@gmail.com>
2025-09-25 15:56:22 +0200

    Renamed method

--- exchange/src/main/java/com/jonyshev/exchange/config/SecurityConfig.java
+++ exchange/src/main/java/com/jonyshev/exchange/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain api(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(reg -> reg
                 .requestMatchers("/actuator/health", "/api/**").permitAll()
