e3901d75f025da9803dc920416a07333d2fe4149
jonyShev <jony.a.shev@gmail.com>
2025-09-26 17:18:46 +0200

    Added 2 methods getUserProfile, auth

--- front-ui/src/main/java/com/jonyshev/front/service/AccountsClient.java
+++ front-ui/src/main/java/com/jonyshev/front/service/AccountsClient.java
@@ -1,6 +1,7 @@
 package com.jonyshev.front.service;
 
 import com.jonyshev.commons.dto.UserCreateRequest;
+import com.jonyshev.commons.dto.UserProfileDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,26 @@ public class AccountsClient {
                 .exchangeToMono(r -> Mono.just(r.statusCode().is2xxSuccessful()))
                 .blockOptional()
                 .orElse(false);
+    }
+
+    public UserProfileDto getUserProfile(String login) {
+        return http.build()
+                .get()
+                .uri("http://accounts/api/users/{login}", login)
+                .retrieve()
+                .bodyToMono(UserProfileDto.class)
+                .block();
+    }
+
+    public boolean auth(String login, String password) {
+        return Boolean.TRUE.equals(
+                http.build()
+                .post()
+                .uri("http://accounts/api/internal/auth?login={login}&password={password}", login, password)
+                .retrieve()
+                .toBodilessEntity()
+                .map(resp -> resp.getStatusCode().is2xxSuccessful())
+                .onErrorReturn(false)
+                .block());
     }
 }
