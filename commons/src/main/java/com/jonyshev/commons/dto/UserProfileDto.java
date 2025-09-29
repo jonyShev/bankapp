78566a74c39654d2384fd966853b7710f77c0e63
jonyShev <jony.a.shev@gmail.com>
2025-09-29 14:16:18 +0200

    Added Builder

--- commons/src/main/java/com/jonyshev/commons/dto/UserProfileDto.java
+++ commons/src/main/java/com/jonyshev/commons/dto/UserProfileDto.java
@@ -1,12 +1,14 @@
 package com.jonyshev.commons.dto;
 
 import com.jonyshev.commons.model.Currency;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@Builder
 public class UserProfileDto {
     private String login;
     private String name;
