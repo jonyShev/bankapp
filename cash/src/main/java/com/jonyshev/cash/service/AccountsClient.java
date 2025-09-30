19ea47bca953656d1ed1024714a9ef72bc17c667
jonyShev <jony.a.shev@gmail.com>
2025-09-30 15:10:50 +0200

    Added AccountsClient in cash

---
+++ cash/src/main/java/com/jonyshev/cash/service/AccountsClient.java
@@ -0,0 +1,44 @@
+package com.jonyshev.cash.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class AccountsClient {
+
+    private final WebClient.Builder http;
+
+    private WebClient client() {
+        return http.baseUrl("http://accounts/api/internal").build();
+    }
+
+    public boolean add(String login, String currency, String amount) {
+        return postWithParams("/add", login, currency, amount);
+    }
+
+    public boolean sub(String login, String currency, String amount) {
+        return postWithParams("/sub", login, currency, amount);
+    }
+
+    private boolean postWithParams(String path, String login, String currency, String amount) {
+        Mono<ResponseEntity<Void>> mono = client()
+                .post()
+                .uri((UriBuilder uri) -> uri.path(path)
+                        .queryParam("login", login)
+                        .queryParam("currency", currency)
+                        .queryParam("amount", amount)
+                        .build())
+                .retrieve()
+                .toBodilessEntity();
+
+        return Boolean.TRUE.equals(mono
+                .map(resp -> resp.getStatusCode().is2xxSuccessful())
+                .onErrorReturn(false)
+                .block());
+    }
+}
