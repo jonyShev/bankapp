ca1ca35e364e0c78a9eea8cfaf6d19a85566521e
jonyShev <jony.a.shev@gmail.com>
2025-10-01 17:04:16 +0200

    Reafctoring

--- transfer/src/main/java/com/jonyshev/transfer/TransferApplication.java
+++ transfer/src/main/java/com/jonyshev/transfer/TransferApplication.java
@@ -1,4 +1,4 @@
-package com.jonyshev.exchange;
+package com.jonyshev.transfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
