d9f60b7a34cf6068c8574bee8e6fec1c15cd4479
jonyShev <jony.a.shev@gmail.com>
2025-10-01 16:56:11 +0200

    Refactoring

--- cash/src/main/java/com/jonyshev/cash/service/AccountsClient.java
+++ cash/src/main/java/com/jonyshev/cash/service/AccountsClient.java
@@ -2,12 +2,14 @@ package com.jonyshev.cash.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
 
-@Service
+import java.math.BigDecimal;
+
+@Component
 @RequiredArgsConstructor
 public class AccountsClient {
 
@@ -17,21 +19,21 @@ public class AccountsClient {
         return http.baseUrl("http://accounts/api/internal").build();
     }
 
-    public boolean add(String login, String currency, String amount) {
+    public boolean add(String login, String currency, BigDecimal amount) {
         return postWithParams("/add", login, currency, amount);
     }
 
-    public boolean sub(String login, String currency, String amount) {
+    public boolean sub(String login, String currency, BigDecimal amount) {
         return postWithParams("/sub", login, currency, amount);
     }
 
-    private boolean postWithParams(String path, String login, String currency, String amount) {
+    private boolean postWithParams(String path, String login, String currency, BigDecimal amount) {
         Mono<ResponseEntity<Void>> mono = client()
                 .post()
                 .uri((UriBuilder uri) -> uri.path(path)
                         .queryParam("login", login)
                         .queryParam("currency", currency)
-                        .queryParam("amount", amount)
+                        .queryParam("amount", amount.toPlainString()) // безопасная конверсия
                         .build())
                 .retrieve()
                 .toBodilessEntity();
