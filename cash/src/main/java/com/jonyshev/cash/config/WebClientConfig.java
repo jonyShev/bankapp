7cae2d04ec581726f220be37812323dc18429ab8
jonyShev <jony.a.shev@gmail.com>
2025-10-08 19:49:56 +0200

    Added filter in WebClientConfig

--- cash/src/main/java/com/jonyshev/cash/config/WebClientConfig.java
+++ cash/src/main/java/com/jonyshev/cash/config/WebClientConfig.java
@@ -4,14 +4,35 @@ import com.jonyshev.commons.client.NotificationsClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
     @Bean
     @LoadBalanced
-    WebClient.Builder http() {
-        return WebClient.builder();
+    WebClient.Builder webClientBuilder(ServletOAuth2AuthorizedClientExchangeFilterFunction oauth) {
+        return WebClient.builder().filter(oauth);
+    }
+
+    @Bean
+    ServletOAuth2AuthorizedClientExchangeFilterFunction oauth(
+            ClientRegistrationRepository clients,
+            OAuth2AuthorizedClientService svc
+    ) {
+        var manager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clients, svc);
+        var provider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        manager.setAuthorizedClientProvider(provider);
+
+        var oauth = new ServletOAuth2AuthorizedClientExchangeFilterFunction(manager);
+        oauth.setDefaultClientRegistrationId("cash");
+        return oauth;
     }
 
     @Bean
--- front-ui/src/main/java/com/jonyshev/front/config/WebClientConfig.java
+++ front-ui/src/main/java/com/jonyshev/front/config/WebClientConfig.java
@@ -3,14 +3,39 @@ package com.jonyshev.front.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    @Bean
+
+    @Bean("securedWebClientBuilder")
     @LoadBalanced
-    WebClient.Builder http() {
+    WebClient.Builder securedWebClientBuilder(ServletOAuth2AuthorizedClientExchangeFilterFunction oauth) {
+        return WebClient.builder().filter(oauth);
+    }
+
+    @Bean("plainWebClientBuilder")
+    @LoadBalanced
+    WebClient.Builder plainWebClientBuilder() {
         return WebClient.builder();
     }
 
+    @Bean
+    ServletOAuth2AuthorizedClientExchangeFilterFunction oauth(
+            ClientRegistrationRepository clients,
+            OAuth2AuthorizedClientService svc
+    ) {
+        var manager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clients, svc);
+        manager.setAuthorizedClientProvider(
+                OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
+        );
+        var oauth = new ServletOAuth2AuthorizedClientExchangeFilterFunction(manager);
+        oauth.setDefaultClientRegistrationId("front-ui");
+        return oauth;
+    }
 }
